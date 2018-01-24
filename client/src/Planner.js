eae9e32c32f1a238f37f09611934c5348e9282be
jacquelynmarcella <jacquelyn.marcella@gmail.com>
2018-01-24 14:45:24 -0800

    Moved functions from Search component to Planner component, passing data from Planner through to Search and Results components

--- client/src/Planner.js
+++ client/src/Planner.js
@@ -2,14 +2,63 @@ import React, {Component} from 'react';
 import Search from './Search.js';
 import Results from './Results.js';
 import MapContainer from './MapContainer.js';
+import geocoder from 'geocoder';
+import axios from 'axios';
+import moment from 'moment';
 
 class Planner extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			locationA: '',
+			locationB: '',
+			sunsetTime: '',
+			lat: '',
+			lng: ''
+		}
+	}
+
+	updateLocation = (event) => {
+		//Will update location upon change of the text fields
+		this.setState({ 
+			[event.target.name]: event.target.value
+		});
+	}
+
+	fetchSunset = (event) => {
+		let latitude;
+		let longitude;
+		let base = this;
+
+		event.preventDefault();
+		console.log("Fetch the sunset time traveling from " + this.state.locationA + " to " + this.state.locationB);
+
+		geocoder.geocode(this.state.locationB, function(err,data){
+			latitude = data.results[0].geometry.location.lat;
+			longitude = data.results[0].geometry.location.lng;
+			console.log(latitude, longitude);
+
+			let sunsetApi = 'https://api.sunrise-sunset.org/json?lat='+latitude+'&lng='+longitude+'&date=today'
+
+			axios.get(`${sunsetApi}`)
+				.then(({data}) => {
+					console.log(data);
+					base.setState({
+						lat: latitude,
+						lng: longitude,
+						sunsetTime: data.results.sunset
+					})
+				})
+
+		})
+	}
+
     render(){
         return(
         <div className='planner'>
-				<Search />
-				<Results sunsetTime="SunsetTime state/prop" arrivalTime="Arrival Time" />
-        <MapContainer />
+			<Search data={this.state} updateLocation={this.updateLocation} fetchSunset={this.fetchSunset} />
+			<Results data={this.state} />
+        	<MapContainer />
         </div>
         );
     }
