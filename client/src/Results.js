85aedfaee4bdd96a057118612b9c27607a86c576
seanthaswan <sean-swan@hotmail.com>
2018-01-24 12:49:31 -0800

    Stubbed out Results.js

--- client/src/Results.js
+++ client/src/Results.js
@@ -5,7 +5,10 @@ class Results extends Component {
   render(){
   return(
     <div>
-      Results
+      <p className="p--results__sunsettime">The Sun will set at your destination at "TIME GOES HERE"</p>
+      <p className="p--results__commutetime">If you head out now, you will arive at "TIME"</p>
+      <p className="p--results__true">You will make it before the sun has set! :) </p>
+      <p className="p--results__false">You will not make it before the sun has set! :( </p>
     </div>
     );
   } 
