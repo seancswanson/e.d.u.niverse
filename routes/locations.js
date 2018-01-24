777871af80baa2639bd4735c41d9db1342964ce7
jacquelynmarcella <jacquelyn.marcella@gmail.com>
2018-01-24 15:44:52 -0800

    Working on locations backend

--- routes/locations.js
+++ routes/locations.js
@@ -3,4 +3,8 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var User = require('../models/user');
 
+router.post('/', function(req, res, next){
+	res.send("Post route reached")
+})
+
 module.exports = router;
