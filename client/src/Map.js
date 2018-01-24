a0551692c1f8cf038fba2897560dcff749c52a98
RAnderso0325 <reillyjanderson@gmail.com>
2018-01-24 13:04:15 -0800

    redoing map component

--- client/src/Map.js
+++ client/src/Map.js
@@ -1,13 +1,6 @@
 import React, { Component } from 'react';
-import initMap from './InitMap.js';
 
 class Map extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            map: ''
-        }
-    }
     
   render(){
     var mapURL="https://maps.googleapis.com/maps/api/js?key={process.env.local.GOOGLE_MAPS_API}&callback=initMap"
@@ -15,7 +8,7 @@ class Map extends Component {
     // if brower support available, ask user for location data and set the map view
       return(
         <div>
-            <initMap map={this.props.map}/>
+            
         </div>
       );
   }
