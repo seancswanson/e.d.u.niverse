19efd9c66fb1f7868589ae42ba764bdb6ef1bcc4
seanthaswan <sean-swan@hotmail.com>
2018-01-24 18:35:18 -0800

    added classes to style

--- client/src/Profile.js
+++ client/src/Profile.js
@@ -22,7 +22,7 @@ class Profile extends Component {
 
   render(){
     if(this.props.user && this.props.user.name){
-      return (<div>
+      return (<div className="Profile">
           <h2>HELLO AGAIN {this.props.user.name}!</h2>
           <h4>Your email is {this.props.user.email}</h4>
           <p>Locations: {this.state.locations}</p>
