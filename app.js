b5f2bee25fb1fa1e9ed8332b95870fee9fc15081
Sean Swanson <sean-swan@hotmail.com>
2018-03-19 16:36:04 -0700

    Reformatted styles, stubbed new dashboard component.

--- app.js
+++ app.js
@@ -8,28 +8,26 @@ var app = express();
 
 // Mongoose stuff
 var mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/MERN', {useMongoClient: true});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/MERN', { useMongoClient: true });
 
 // Set up middleware
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json({limit: "50mb"}));
-app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
 app.use(express.static(path.resolve(__dirname, 'client', 'build')));
 
 app.use(function(req, res, next) {
-  // before every route, attach the flash messages and current user to res.locals
-  res.locals.currentUser = req.user;
-  next();
+    // before every route, attach the flash messages and current user to res.locals
+    res.locals.currentUser = req.user;
+    next();
 });
 
 // Controllers
 app.use('/auth', require('./routes/auth'));
-app.use('/map', require('./routes/map'));
-app.use('/locations', require('./routes/locations'));
 
 app.get('*', function(req, res, next) {
-	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
 module.exports = app;
